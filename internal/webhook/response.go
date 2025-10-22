1478ea92fc58a449fb064c39fa8467aff5565d43
Jose Szychowski <joseszychowski@gmail.com>
2025-10-22 11:21:55 -0300

    chore: update milo dependency to v0.6.1 and add extra information to authentication response

--- internal/webhook/response.go
+++ internal/webhook/response.go
@@ -1,23 +1,24 @@
 package webhook
 
 import (
+	iammiloapiscomv1alpha1 "go.miloapis.com/milo/pkg/apis/iam/v1alpha1"
 	authenticationv1 "k8s.io/api/authentication/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Denied(reason string) Response {
-	return authenticationResponse(false, "", "", reason)
+	return authenticationResponse(false, "", "", reason, "")
 }
 
 func Errored(err error) Response {
-	return authenticationResponse(false, "", "", err.Error())
+	return authenticationResponse(false, "", "", err.Error(), "")
 }
 
-func Allowed(username, uid string) Response {
-	return authenticationResponse(true, username, uid, "")
+func Allowed(username, uid string, registrationApproval iammiloapiscomv1alpha1.RegistrationApprovalState) Response {
+	return authenticationResponse(true, username, uid, "", registrationApproval)
 }
 
-func authenticationResponse(authenticated bool, username, uid, evaluationError string) Response {
+func authenticationResponse(authenticated bool, username, uid, evaluationError string, registrationApproval iammiloapiscomv1alpha1.RegistrationApprovalState) Response {
 	return Response{
 		TokenReview: authenticationv1.TokenReview{
 			TypeMeta: metav1.TypeMeta{
@@ -29,6 +30,9 @@ func authenticationResponse(authenticated bool, username, uid, evaluationError s
 				User: authenticationv1.UserInfo{
 					Username: username,
 					UID:      uid,
+					Extra: map[string]authenticationv1.ExtraValue{
+						"iam.miloapis.com/registrationApproval": {string(registrationApproval)},
+					},
 				},
 				Error: evaluationError,
 			},
